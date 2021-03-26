edd62de572b3707fad1c62f3617b99a9bf48fe2e
Mirela <adisserdarevic@gmail.com>
2021-03-26 06:40:00 +0100

    Added file index.js

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,13 +1,10 @@
 let numberArray = new Set ([9, 5, 4, 11, 5, 12, 13, 15, 4, 2, 5]);
-
+// Only change code below this line
 let uniqueArray = [];
-
-
-
 for(let set of numberArray) {
     uniqueArray.push(set);
-    
 }
+// Only change code above this line
 uniqueArray.sort((a,b) => a - b);
 console.log(uniqueArray);
 module.exports = { numberArray, uniqueArray };
